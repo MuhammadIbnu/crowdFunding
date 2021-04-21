535ab2d6df13c1fc1c4a40733c2f1ca0fe41cf4b
MuhammadIbnu <m.ibnu96@gmail.com>
2021-04-21 22:30:29 +0700

    profile

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,18 @@ Route::prefix('admin')->group(function () {
 
         //route dashboard
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+        //route Category
+        Route::resource('/category', CategoryController::class,['as'=>'admin']);
+        //route Campaign
+        Route::resource('/campaign', CampaignController::class,['as'=>'admin']);
+        //get route donatur
+        Route::get('/donatur', [DonaturController::class,'index'])->name('admin.donatur.index');
+        //get route donasi
+        Route::get('/donasi', [DonationController::class,'index'])->name('admin.donation.index');
+        //get filter donasi
+        Route::get('/donasi/filter',[DonationController::class,'filter'])->name('admin.donation.filter');
+        //get profile
+        Route::get('/profile',[ProfileController::class,'index'])->name('admin.profile.index');
 
     });
 });
-//route Category
-Route::resource('/category', CategoryController::class,['as'=>'admin']);
-//route Campaign
-Route::resource('/campaign', CampaignController::class,['as'=>'admin']);
-//get route donatur
-Route::get('/donatur', [DonaturController::class,'index'])->name('admin.donatur.index');
-//get route donasi
-Route::get('/donasi', [DonationController::class,'index'])->name('admin.donation.index');
-//get filter donasi
-Route::get('/donasi/filter',[DonationController::class,'filter'])->name('admin.donation.filter');
-//get profile
-Route::get('/profile',[ProfileController::class,'index'])->name('admin.profile.index');
