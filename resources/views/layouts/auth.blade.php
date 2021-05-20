22036acafc61cd565a240f2de6702079c781fd46
MuhammadIbnu <m.ibnu96@gmail.com>
2021-05-20 14:18:43 +0700

    typo

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,11 +9,11 @@
         <link rel="shortcut icon" type="image/jpg" href="https://i.imgur.com/UyXqJLi.png" />
         <title>{{ $title }}</title>
         <!-- CSS -->
-        <link rel="stylesheet" href="{{ 'css/app.css'}}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <!-- JS -->
-        <script src="{{ 'js/app.js' }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
         <!-- content -->
